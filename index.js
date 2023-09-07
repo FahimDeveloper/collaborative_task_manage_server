1a490754fc5c7409e7e55bdd27c216ff912cfb86
FahimDeveloper <developerfahim246@gmail.com>
2023-09-07 11:37:35 +0600

    initial setup completed

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", async (req, res) => {
+    res.send("Collaborative task management server running")
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
